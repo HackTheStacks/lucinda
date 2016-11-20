31cfd5f7fdd338924ee7c628d972a58b9470977f
Jonathan Pevarnek <pevarnj@gmail.com>
2016-11-20 02:47:38 -0500

    Remove maxage cache setting

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ app.use((req, res, next) => {
   }
   next();
 });
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 /**
  * Primary app routes.
